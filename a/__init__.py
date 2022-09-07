42b3af0b29cb77e7974efc9c71c8a36a0c61f708
MingxuanAWA <84988050+MingxuanAWA@users.noreply.github.com>
2022-09-07 18:06:17 +0800

    Update __init__.py

--- a/__init__.py
+++ a/__init__.py
@@ -2,7 +2,7 @@ import json
 import asyncio
 import httpx
 async def fetch():
-    async with httpx.AsyncClient() as c:
+    async with         httpx.AsyncClient() as c:
         r= await c.get("https://httpbin.com/get")
         return r.json()  
 if __name__ == "__main__":
